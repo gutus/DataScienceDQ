fb036e8b70cbe7de877685a110ef837210296bab
Gutus Nirwanto <gutusnirwanto@gmail.com>
2022-07-10 01:57:37 +0700

    first commit

---
+++ BasicDSPython/csvReader.py
@@ -0,0 +1,19 @@
+from contextlib import closing
+import csv
+import requests
+
+
+# tentukan lokasi file, nama file, dan inisialisasi csv
+url = 'https://storage.googleapis.com/dqlab-dataset/penduduk_gender_head.csv'
+
+# baca file csv secara streaming
+with closing(requests.get(url, stream=True)) as r:
+    f = (line.decode('utf-8') for line in r.iter_lines())
+
+    reader = csv.reader(f, delimiter=',')
+
+# membaca baris per baris
+    for row in reader:
+        print(row)
+# menutup file csv
+f.close()
